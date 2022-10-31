f44480ee9e3d071a18e8fec013fccf75ca4ceb5b
SadBlueYet <88283979+SadBlueYet@users.noreply.github.com>
2022-11-01 04:47:12 +0700

    Задание 2

---
+++ task2.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <fstream>
+
+int num(int a)
+{
+	if (a < 10 && a >= 0)
+	{
+		return 1;
+	}
+	else if (a > 9 && a <= 99)
+	{
+		return 2;
+	}
+	else if (a > 99 && a <= 999)
+	{
+		return 3;
+	}
+	else
+		return 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	std::ifstream fin;
+	std::ofstream fout;
+	std::ofstream result("result.txt");
+
+	int number;
+	int numberInTheFile;
+
+	do 
+	{
+		std::cout << "Введите положительное число:";
+		std::cin >> number;
+
+		if (number != 0)
+		{
+			fout.open("number.txt", std::ofstream::app);
+
+			if (!fout.is_open())
+			{
+				std::cout << "Ошибка открытия файла";
+				return 1;
+			}
+			else
+			{
+				fout << number << '\n';
+				fout.close();
+			}
+
+		}
+
+	}while (number != 0);
+
+	fin.open("number.txt", std::ios_base::in);
+
+	if (!fin.is_open())
+	{
+		std::cout << "Ошибка открытия файла";
+		return 1;
+	}
+	else
+	{
+		while (fin >> numberInTheFile)
+		{
+
+			if (num(numberInTheFile) == 1)
+			{
+				result << "Число " << numberInTheFile << " Однозначаное" << '\n';
+			}
+			else if (num(numberInTheFile) == 2)
+			{
+				result << "Число " << numberInTheFile << " Двухзначное" << '\n';
+			}
+			else if (num(numberInTheFile))
+			{
+				result << "Число " << numberInTheFile << " Трехзначное" << '\n';
+			}
+			else if (numberInTheFile > 999)
+			{
+				std::cout << "Число " << numberInTheFile << " больше 999" << '\n';
+			}
+			else
+			{
+				std::cout << "Число " << numberInTheFile << " отрицательное" << '\n';
+			}
+
+
+		}
+		fin.close();
+	}
+	return 0;
+} 
+
